1ff89c53be30bf2127c71708346f3ad80084fcbb
Sergey Nabatov <serynabatov@gmail.com>
2020-11-28 14:30:51 +0300

    add userservice not much but it's an honest work!

---
+++ GamifiedMarketingAppEJB/src/services/UserService.java
@@ -0,0 +1,44 @@
+package services;
+
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.*;
+
+import entities.User;
+import exceptions.CredentialsException;
+import it.polimi.db2.album.services.NonUniqueResultException;
+import it.polimi.db2.album.services.PersistenceException;
+
+@Stateless
+public class UserService {
+
+	// annotation PersistenceContext() declare a dependency on a persistence context
+	// so we don't need EntityManagerFactory
+	// PersistenceContext is a set of managed entity instances within an entity manager at
+	// any given time
+	@PersistenceContext(unitName = "GamifiedMarketingAppEJB")
+	private EntityManager em;
+	
+	public UserService() { }
+	
+	public User checkCredentials(String username, String password) 
+			throws CredentialsException, NonUniqueResultException{
+		
+		List<User> uList = null;
+		try {
+			uList = em.createNamedQuery("User.checkCredentials", User.class)
+					   .setParameter(1, username).setParameter(2, password)
+					   .getResultList();
+		} catch (PersistenceException e) {
+			throw new CredentialsException("Could not verify credentals");
+		}
+		if (uList.isEmpty())
+			return null;
+		else if (uList.size() == 1)
+			return uList.get(0);
+		throw new NonUniqueResultException("More than one user registered with same credentials");
+	}
+	
+}
