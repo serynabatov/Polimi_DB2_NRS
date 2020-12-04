d047c1478a66c88d604e9f8f1a606cb54d9e0f1d
Alex <bralix@mail.ru>
2020-12-05 00:42:24 +0300

    Projects merging

---
+++ RequeltHandler/src/it/mirea/marketing/website/pages/Home.java
@@ -0,0 +1,26 @@
+package it.mirea.marketing.website.pages;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import services.ProductOfTheDayService;
+
+@WebServlet("/HomePage")
+public class Home extends HttpServlet {
+	@EJB(name = "entities/ProductOfTheDayService")
+	ProductOfTheDayService productOfTheDayService;
+	
+	private static final long serialVersionUID = 1L;  
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print(productOfTheDayService.getNameImage().toString());	
+		//response.getWriter().append("Served at: ").append(request.getContextPath());	
+	}
+
+}
